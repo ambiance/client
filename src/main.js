74e651f3af345da44ae3ba26707b108de4068356
Jonathan Azali <azali.jonathan@gmail.com>
2019-03-05 09:41:56 -0800

    modified js for automating card creation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,41 @@
-import { setActiveToggle } from "./components/page-header/PageHeader.js";
-import showSlide from "./components/slideshow/slideshow.js";
+import { setActiveToggle } from './components/page-header/PageHeader.js';
+import showSlide from './components/slideshow/slideshow.js';
 
 // setActiveToggle();
 showSlide(0);
+
+document
+  .getElementById('submitButton')
+  .addEventListener('click', insertResults);
+
+const resultsCardsObject = document.querySelector('.resultCards');
+// const resultsNum = WhateverBackendGives();
+const resultsNum = 5;
+
+function insertResults() {
+  resultsCardsObject.innerHTML = getResults();
+}
+
+function getResults() {
+  let resultsCardsHTML = '';
+
+  for (let i = 0; i < resultsNum; i++) {
+    resultsCardsHTML += `
+  <div class="resultCard">
+    <div class="resultCardImageContainer">
+      <img
+        class="resultCardImage"
+        src="https://www.elastic.co/assets/bltada7771f270d08f6/enhanced-buzz-1492-1379411828-15.jpg"
+      />
+    </div>
+    <p class="resultCardTitle">
+      Address
+    </p>
+    <p class="resultCardSubtitle">
+      Aura
+    </p>
+  </div>`;
+  }
+
+  return resultsCardsHTML;
+}
