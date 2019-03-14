f7c8932c43c181a4cc22bef1ddc1c6f2e465f6d9
emily-yang <cigoua@gmail.com>
2019-03-14 14:44:33 -0700

    convert About page to React

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
-import React from "react";
-import "../css/header.css";
-import auraLogo from "../assets/img/auraLogo.png";
+import React from 'react';
+import '../css/header.css';
+import auraLogo from '../assets/img/auraLogo.png';
 
 class Footer extends React.Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 // import AuraApp from "./components/AuraApp";
 
 // Delete below
-import Home from "./components/Home";
+import Home from './components/Home';
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+ReactDOM.render(<Home />, document.getElementById('root'));
