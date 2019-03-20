515cd7738b04326ab98234041135605ba0ab8897
emily-yang <cigoua@gmail.com>
2019-03-20 09:29:45 -0700

    fix logo link to homepage

--- src/components/AuraApp.js
+++ src/components/AuraApp.js
@@ -1,11 +1,9 @@
-import React from "react";
-import {
- BrowserRouter, Switch, Route, NavLink 
-} from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
-import MeetTheTeam from "./MeetTheTeam";
-import auraLogo from "../assets/img/auraLogo.png";
+import React from 'react';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import MeetTheTeam from './MeetTheTeam';
+import auraLogo from '../assets/img/auraLogo.png';
 
 class AuraApp extends React.Component {
   render() {
@@ -18,14 +16,9 @@ class AuraApp extends React.Component {
       <BrowserRouter>
         <div className="App">
           <header>
-            <a href="index.html">
-              <img
-                src={auraLogo}
-                alt="auraLogo"
-                className="headerLogo"
-                href="#home"
-              />
-            </a>
+            <NavLink to="/">
+              <img src={auraLogo} alt="auraLogo" className="headerLogo" href="#home" />
+            </NavLink>
 
             <nav>
               <ul>
