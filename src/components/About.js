57665a915aad7d72f2dd599342060fda90da2dec
Devon Marantz <DMarantz@talentpath.com>
2019-03-29 09:08:14 -0700

    Make Brocks Money Moves

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ class About extends React.Component {
     return (
       <div className="about">
         <Slideshow />
-        <section className="icons">
+        {/* <section className="icons">
           <img
             className="icon"
             src="https://media.discordapp.net/attachments/540283031001235477/560874061996228628/People_White.png?width=512&height=450"
@@ -21,9 +21,13 @@ class About extends React.Component {
             className="icon"
             src="https://media.discordapp.net/attachments/540283031001235477/560868650899341351/WhiteJesus.png?width=450&height=450"
           />
-        </section>
+        </section> */}
         <section className="content">
           <section className="mint landing">
+            <img
+              className="icon"
+              src="https://media.discordapp.net/attachments/540283031001235477/560874061996228628/People_White.png?width=512&height=450"
+            />
             <p className="minty">Everyone is unique.</p>
             <p className="minty">
               People like different things and want to feel great wherever they
@@ -31,6 +35,10 @@ class About extends React.Component {
             </p>
           </section>
           <section className="mint landing">
+            <img
+              className="icon"
+              src="https://media.discordapp.net/attachments/540283031001235477/560871549674979338/Question_Head_White.png?width=450&height=450"
+            />
             <p className="minty">
               Whether you want to study, socialize, or hold business meetings…
             </p>
@@ -38,11 +46,14 @@ class About extends React.Component {
           </section>
 
           <section className="mint landing">
+            <img
+              className="icon"
+              src="https://media.discordapp.net/attachments/540283031001235477/560868650899341351/WhiteJesus.png?width=450&height=450"
+            />
             <p className="minty">
               We'll show you where to go based on what you want to do and what
               <span className="bold"> vibe</span> you're going for.
             </p>
-            <p className="minty">Aura finds the right vibe for you!</p>
           </section>
         </section>
       </div>
