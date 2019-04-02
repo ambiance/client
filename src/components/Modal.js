ab2c8e008a377a58228893756a2eba3bf0da5a1b
Brock Jameson <bjameson@talentpath.com>
2019-04-02 16:50:17 -0700

    modal info

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import Map from "./Map";
 import "../css/Modal.css";
 
 const modal = props => {
+  console.log(props);
   return (
     <div>
       <div
@@ -13,13 +14,24 @@ const modal = props => {
         }}
       >
         <div className="modal-header">
-          <h3>{props.children}</h3>
+          <h3>{props.show ? props.details.details.name : ""}</h3>
           <button className="close-modal-btn" onClick={props.close}>
             ×
           </button>
         </div>
         <div className="modal-body">
-          <p>{props.children}</p>
+          {/* <Map /> */}
+          <p>{props.show ? props.details.details.displayAddress[0] : ""}</p>
+          <p>{props.show ? props.details.details.displayAddress[1] : ""}</p>
+          <p>{props.show ? props.details.details.attributes.priceRange : ""}</p>
+          <p>{props.show ? props.details.details.stars + " stars" : ""}</p>
+          {/* <p>{props.show ? props.details.details.categories : ""}</p> */}
+          <a
+            className="yelpLink"
+            href={props.show ? props.details.details.url : ""}
+          >
+            Link to Yelp!
+          </a>
         </div>
         <div className="modal-footer" />
       </div>
