4c0a82705527cc54bcdcf5278ee2d495a5b1e87c
Devon Marantz <DMarantz@talentpath.com>
2019-04-08 16:13:20 -0700

    Make the map relative to modal

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,11 @@
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
-import React from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import React from 'react';
 
 export class MapContainer extends React.Component {
   render() {
     return (
       <Map google={this.props.google} zoom={14}>
-        <Marker onClick={this.onMarkerClick} name={"Current location"} />
+        <Marker onClick={this.onMarkerClick} name="Current location" />
 
         <InfoWindow onClose={this.onInfoWindowClose}>
           <div>
@@ -18,7 +18,7 @@ export class MapContainer extends React.Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo"
+  apiKey: 'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo',
 })(MapContainer);
 
 // ===============================================================================================================================
