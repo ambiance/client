9264aae1201968456c186a597d4c63e19e97c21d
Devon Marantz <DMarantz@talentpath.com>
2019-04-11 10:02:51 -0700

    Fix aura pill merge

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,24 +1,20 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 // import CardItem from './CardItem';
-import Map from "./Map";
-import MapContainer from "./Map";
-import "../css/Modal.css";
-import "../css/palette.css";
-import starImages from "./starImages";
+import Map from './Map';
+import MapContainer from './Map';
+import '../css/Modal.css';
+import '../css/palette.css';
+import starImages from './starImages';
 
 const Modal = props => {
   // console.log(business);
   const starSrc = handleStars(props.show ? props.details.details.stars : 0);
   // const styleObject = auraColorChange(props.details.details.attributes.aura);
   // const newAuras = auraSpace();
-  const styleObject = auraColorChange(
-    props.show ? props.details.details.attributes.aura : ""
-  );
+  const styleObject = auraColorChange(props.show ? props.details.details.attributes.aura : '');
   console.log(styleObject);
-  const array = auraSpace(
-    props.show ? props.details.details.attributes.aura : ""
-  );
+  const array = auraSpace(props.show ? props.details.details.attributes.aura : '');
   // const auras = colorChange.split(',');
   // const arrAuras = auras.join(', ');
   function auraSpace() {
@@ -27,35 +23,35 @@ const Modal = props => {
   }
 
   function auraColorChange(auraString) {
-    const auras = auraString.split(",");
-    let colorString = "";
+    const auras = auraString.split(',');
+    let colorString = '';
     for (let i = 0; i < auras.length; i++) {
       switch (auras[i]) {
-        case "trendy":
+        case 'trendy':
           colorString = `${colorString}var(--trendy), `;
           break;
-        case "romantic":
+        case 'romantic':
           colorString = `${colorString}var(--romantic), `;
           break;
-        case "hipster":
+        case 'hipster':
           colorString = `${colorString}var(--hipster), `;
           break;
-        case "casual":
+        case 'casual':
           colorString = `${colorString}var(--casual), `;
           break;
-        case "inspired":
+        case 'inspired':
           colorString = `${colorString}var(--inspired), `;
           break;
-        case "intimate":
+        case 'intimate':
           colorString = `${colorString}var(--intimate), `;
           break;
-        case "classy":
+        case 'classy':
           colorString = `${colorString}var(--classy), `;
           break;
-        case "touristy":
+        case 'touristy':
           colorString = `${colorString}var(--touristy), `;
           break;
-        case "cheerful":
+        case 'cheerful':
           colorString = `${colorString}var(--cheerful), `;
           break;
         default:
@@ -77,77 +73,57 @@ const Modal = props => {
         onClick={props.close}
         style={{
           // transform: props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
-          opacity: props.show ? "0.5" : "0",
-          position: props.show ? "fixed" : "absolute"
+          opacity: props.show ? '0.5' : '0',
+          position: props.show ? 'fixed' : 'absolute',
+          zIndex: props.show ? '15' : '-5',
         }}
       />
       <div
         className="modal-wrapper"
         style={{
-          transform: props.show ? "translateY(0vh)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0"
+          transform: props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
+          opacity: props.show ? '1' : '0',
         }}
       >
         <div className="modal-header">
-          <h3>{props.show ? props.details.details.name : ""}</h3>
+          <h3>{props.show ? props.details.details.name : ''}</h3>
           {/* {props.details.details.attributes.aura.map((aura, i) => (
             <span>
               <Pill key={i} aura={aura} />
             </span>
           ))} */}
-          <h1 style={styleObject}>
-            {props.show ? props.details.details.attributes.aura : ""}
-          </h1>
+          <h1 style={styleObject}>{props.show ? props.details.details.attributes.aura : ''}</h1>
           <button className="close-modal-btn" onClick={props.close}>
             ×
           </button>
         </div>
-        <div className="modal-body">
-          <div className="businessMap">
-            <MapContainer
-              className="modalMap"
-              details={props.details.details}
-            />
-          </div>
-          <div className="businessDetails">
-            <ul className="categories">
-              {props.show
-                ? props.details.details.categories.map(category => (
-                    <li>{category.title}</li>
-                  ))
-                : ""}
-            </ul>
-            <p className="info">
-              {props.show ? props.details.details.displayAddress[0] : ""}
-            </p>
-            <p className="info">
-              {props.show ? props.details.details.displayAddress[1] : ""}
-            </p>
-            <p className="info">
-              {props.show ? props.details.details.attributes.priceRange : ""}
-            </p>
-            <img className="modalStar" src={starSrc} />
-            <a
-              className="yelpLink"
-              href={props.show ? props.details.details.url : ""}
-              target="_blank"
-            >
-              <img className="yelpPic" src="./assets/img/yelpButton.jpg" />
-              Click for more details!
-              {/* <p className="yelpCall">Click for more details!</p> */}
-            </a>
-          </div>
-          <div className="modal-footer">
-            <button id="trendybtn">Trendy</button>
-            <button id="inspiredbtn">Inspired</button>
-            <button id="romanticbtn">Romantic</button>
-            <button id="cheerfulbtn">Cheerful</button>
-            <button id="intimatebtn">Intimate</button>
-            <button id="classybtn">Classy</button>
-            <button id="hipsterbtn">Hipster</button>
-            <button id="casualbtn">Casual</button>
-            <button id="touristybtn">Touristy</button>
-          </div>
+        <div className="businessMap">
+          <MapContainer className="modalMap" details={props.details.details} />
+        </div>
+        <div className="businessDetails">
+          <ul className="categories">
+            {props.show ? props.details.details.categories.map(category => <li>{category.title}</li>) : ''}
+          </ul>
+          <p className="info">{props.show ? props.details.details.displayAddress[0] : ''}</p>
+          <p className="info">{props.show ? props.details.details.displayAddress[1] : ''}</p>
+          <p className="info">{props.show ? props.details.details.attributes.priceRange : ''}</p>
+          <img className="modalStar" src={starSrc} />
+          <a className="yelpLink" href={props.show ? props.details.details.url : ''} target="_blank">
+            <img className="yelpPic" src="./assets/img/yelpButton.jpg" />
+            Click for more details!
+            {/* <p className="yelpCall">Click for more details!</p> */}
+          </a>
+        </div>
+        <div className="modal-footer">
+          <button id="trendybtn">Trendy</button>
+          <button id="inspiredbtn">Inspired</button>
+          <button id="romanticbtn">Romantic</button>
+          <button id="cheerfulbtn">Cheerful</button>
+          <button id="intimatebtn">Intimate</button>
+          <button id="classybtn">Classy</button>
+          <button id="hipsterbtn">Hipster</button>
+          <button id="casualbtn">Casual</button>
+          <button id="touristybtn">Touristy</button>
         </div>
       </div>
     </div>
@@ -189,9 +165,9 @@ Modal.propTypes = {
     state: PropTypes.string.isRequired,
     postalCode: PropTypes.string.isRequired,
     attributes: PropTypes.shape({
-      aura: PropTypes.string.isRequired
-    }).isRequired
-  }).isRequired
+      aura: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 export default Modal;
