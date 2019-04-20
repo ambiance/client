500f055c77fd50e08c93c1e9002b55b568ff2ec0
Scott Roberts <s.e.roberts0@gmail.com>
2019-04-19 21:07:15 -0700

    Resolve palettes

--- src/data/slideImages.js
+++ src/data/slideImages.js
@@ -1,61 +1,61 @@
 const slideImages = [
   {
-    src: `${process.env.PUBLIC_URL}./assets/img/slideshow/place1.png`,
+    src: `${process.env.PUBLIC_URL}/assets/img/slideshow/place1.png`,
     aura: 'imaginative',
     question: 'What is your aura?',
   },
   {
-    src: `${process.env.PUBLIC_URL}./assets/img/slideshow/place2.jpg`,
+    src: `${process.env.PUBLIC_URL}/assets/img/slideshow/place2.jpg`,
     aura: 'lively',
     question: 'What is your aura?',
   },
   {
-    src: `${process.env.PUBLIC_URL}./assets/img/slideshow/place3.jpg`,
+    src: `${process.env.PUBLIC_URL}/assets/img/slideshow/place3.jpg`,
     aura: 'intimate',
     question: 'What is your aura?',
   },
   {
-    src: `${process.env.PUBLIC_URL}./assets/img/slideshow/place4.jpg`,
+    src: `${process.env.PUBLIC_URL}/assets/img/slideshow/place4.jpg`,
     aura: 'exotic',
     question: 'What is your aura?',
   },
   {
-    src: `${process.env.PUBLIC_URL}./assets/img/slideshow/place5.jpeg`,
+    src: `${process.env.PUBLIC_URL}/assets/img/slideshow/place5.jpeg`,
     aura: 'cheerful',
     question: 'What is your aura?',
   },
   {
-    src: `${process.env.PUBLIC_URL}./assets/img/slideshow/place6.jpg`,
+    src: `${process.env.PUBLIC_URL}/assets/img/slideshow/place6.jpg`,
     aura: 'groovy',
     question: 'What is your aura?',
   },
   {
-    src: `${process.env.PUBLIC_URL}./assets/img/slideshow/place7.jpeg`,
+    src: `${process.env.PUBLIC_URL}/assets/img/slideshow/place7.jpeg`,
     aura: 'inspired',
     question: 'What is your aura?',
   },
   {
-    src: `${process.env.PUBLIC_URL}./assets/img/slideshow/place8.jpeg`,
+    src: `${process.env.PUBLIC_URL}/assets/img/slideshow/place8.jpeg`,
     aura: 'peaceful',
     question: 'What is your aura?',
   },
   {
-    src: `${process.env.PUBLIC_URL}./assets/img/slideshow/place9.jpg`,
+    src: `${process.env.PUBLIC_URL}/assets/img/slideshow/place9.jpg`,
     aura: 'upscale',
     question: 'What is your aura?',
   },
   {
-    src: `${process.env.PUBLIC_URL}./assets/img/slideshow/place10.jpeg`,
+    src: `${process.env.PUBLIC_URL}/assets/img/slideshow/place10.jpeg`,
     aura: 'powerful',
     question: 'What is your aura?',
   },
   {
-    src: `${process.env.PUBLIC_URL}./assets/img/slideshow/place11.jpeg`,
+    src: `${process.env.PUBLIC_URL}/assets/img/slideshow/place11.jpeg`,
     aura: 'classy',
     question: 'What is your aura?',
   },
   {
-    src: `${process.env.PUBLIC_URL}./assets/img/slideshow/place12.jpeg`,
+    src: `${process.env.PUBLIC_URL}/assets/img/slideshow/place12.jpeg`,
     aura: 'hipster',
     question: 'What is your aura?',
   },
