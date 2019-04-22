e19019528a80512f245f926a72c66a2c6fedb2fb
emily-yang <cigoua@gmail.com>
2019-04-22 14:39:11 -0700

    show address in map infowindow

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,38 @@
-import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import React from 'react';
 import PropTypes from 'prop-types';
 
 export class MapContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.markerRef = React.createRef();
+
+    this.state = {
+      showingInfoWindow: true,
+      activeMarker: {},
+    };
+  }
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
+
+  onMapClicked = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null,
+      });
+    }
+  };
+
   render() {
+    const { details } = this.props;
     return (
       <Map
+        onClick={this.onMapClicked}
         google={this.props.google}
         zoom={14}
         center={
@@ -28,7 +55,19 @@ export class MapContainer extends React.Component {
                 }
               : {}
           }
+          ref={this.markerRef}
         />
+        <InfoWindow visible={this.state.showingInfoWindow} marker={this.state.activeMarker}>
+          {details ? (
+            <p>
+              {details.address}
+              <br />
+              {details.city}, {details.state} {details.postalCode}
+            </p>
+          ) : (
+            ''
+          )}
+        </InfoWindow>
       </Map>
     );
   }
