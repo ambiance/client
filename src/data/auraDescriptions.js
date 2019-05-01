dda84ea020b56e657017f10fd09a2fe9a0f789b8
Scott Roberts <s.e.roberts0@gmail.com>
2019-05-01 14:14:44 -0700

    Add Aura Accordion

---
+++ src/data/auraDescriptions.js
@@ -0,0 +1,83 @@
+const auraDescriptions = [
+  {
+    aura: 'imaginative',
+    definition: 'Having or showing creativity or inventiveness.',
+    description: '',
+    examples: [
+      {
+        businessImage: {
+          src:
+            'http://interiorz.co.uk/resource/anu_tuomikoski/1797323073474851676/this-home-one-of-this-weeks-set-ups-was-memorable-full-of-nice-and-imaginative-details-and-a-relaxed-ambiance-wonderfulbohelsinki-mywork-lovemyjob-int.jpg',
+          owner: 'Interiorz',
+        },
+        _id: '5ca4f6b918697e2f007eb5b5',
+      },
+      {
+        businessImage: {
+          src: 'https://images.otstatic.com/prod/26296625/2/large.jpg',
+          owner: 'OpenTable',
+        },
+        _id: '5ca4f6b918697e2f007eb5b5',
+      },
+      {
+        businessImage: {
+          src: 'https://resizer.otstatic.com/v2/photos/large/26002476.jpg',
+          owner: 'OpenTable',
+        },
+        _id: '5ca4f6b918697e2f007eb5b5',
+      },
+    ],
+  },
+  {
+    aura: 'lively',
+    definition: 'A place full of life and high energy.',
+    description: 'An environment that can have loud conversations and movement.',
+    examples: [{}, {}, {}],
+  },
+  {
+    aura: 'intimate',
+    definition: 'Having or showing creativity or inventiveness.',
+    description: '',
+    examples: [{}, {}, {}],
+  },
+  {
+    aura: 'exotic',
+    definition: 'Originating in or characteristic of a distant foreign country.',
+    description: 'The feeling you get when you experience something foreign to your norm.',
+    examples: [{}, {}, {}],
+  },
+  {
+    aura: 'cheerful',
+    definition: 'A place that is full of life and happiness.',
+    description: '',
+    examples: [{}, {}, {}],
+  },
+  {
+    aura: 'inspired',
+    definition: 'Of extraordinary quality, as if arising from some external creative impulse.',
+    description: '',
+    examples: [{}, {}, {}],
+  },
+  {
+    aura: 'upscale',
+    definition: 'Relatively expensive and designed to appeal to affluent consumers..',
+    description:
+      'That feeling when you can say “Pour me another [insert rare vintage here]” and it won’t break the bank.',
+    examples: [{}, {}, {}],
+  },
+  {
+    aura: 'powerful',
+    definition: 'Having great power or strength.',
+    description: "'With great power comes great responsibility' - Ben Parker",
+    examples: [{}, {}, {}],
+  },
+  {
+    aura: 'hipster',
+    definition:
+      'A person who follows the latest trends and fashions, especially those regarded as being outside the cultural mainstream.',
+    description: '',
+    examples: [{}, {}, {}],
+  },
+];
+
+export default auraDescriptions;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Slideshow from '../components/Slideshow';
+import Accordion from '../components/Accordion';
+import auraDescriptions from '../data/auraDescriptions';
 import '../styles/About.scss';
 
+import CardItem from '../components/CardItem';
+
 class About extends React.Component {
+  openModel = business => {
+    alert(business);
+  };
   render() {
     return (
       <div className="about">
         <Slideshow />
+
         <section className="content">
           <section className="mint landing">
             <img
@@ -47,6 +55,37 @@ class About extends React.Component {
             </p>
           </section>
         </section>
+        <Accordion allowMultipleOpen title="Auras">
+          {auraDescriptions.map((item, i) => (
+            <div key={i} label={item.aura}>
+              <h3>{item.definition}</h3>
+              <hr />
+              <p>{item.description}</p>
+              {item.examples.map((business, j) =>
+                business._id ? (
+                  <div className="resultCardImageContainer">
+                    <img
+                      className="resultCardImage"
+                      style={{ width: '10rem', height: '10rem' }}
+                      src={
+                        business.businessImage.src
+                          ? business.businessImage.src
+                          : 'http://mymodernmet.com/wp/wp-content/uploads/2017/10/azuki-camping-hedgehog-3.jpg'
+                      }
+                      alt={
+                        business.businessImage.owner
+                          ? business.businessImage.owner
+                          : 'No image owner provided'
+                      }
+                    />
+                  </div>
+                ) : (
+                  ''
+                )
+              )}
+            </div>
+          ))}
+        </Accordion>
       </div>
     );
   }
