70355d396747ef9fe4b3d64e8ff79b0049c69489
Brock Jameson <bjameson@talentpath.com>
2019-05-31 09:48:40 -0700

    stars fixed with GIMP

--- src/data/starImages.js
+++ src/data/starImages.js
@@ -1,92 +1,92 @@
 const starImages = [
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/0stars.png`,
-    name: '0stars',
+    name: "0stars"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/halfstar.png`,
-    name: 'halfstar',
+    name: "halfstar"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/1star.png`,
-    name: '1star',
+    name: "1star"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/1halfstars.png`,
-    name: '1halfstars',
+    name: "1halfstars"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/2stars.png`,
-    name: '2stars',
+    name: "2stars"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/2halfstars.png`,
-    name: '2halfstars',
+    name: "2halfstars"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/3stars.png`,
-    name: '3stars',
+    name: "3stars"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/3halfstars.png`,
-    name: '3halfstars',
+    name: "3halfstars"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/4stars.png`,
-    name: '4stars',
+    name: "4stars"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/4halfstars.png`,
-    name: '4halfstars',
+    name: "4halfstars"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/5stars.png`,
-    name: '5stars',
+    name: "5stars"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/modal0stars.png`,
-    name: 'modal0stars',
+    name: "modal0stars"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/modalhalfstar.png`,
-    name: 'modalhalfstar',
+    name: "modalhalfstar"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/modal1star.png`,
-    name: 'modal1star',
+    name: "modal1star"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/modal1halfstars.png`,
-    name: 'modal1halfstars',
+    name: "modal1halfstars"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/modal2stars.png`,
-    name: 'modal2stars',
+    name: "modal2stars"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/modal2halfstars.png`,
-    name: 'modal2halfstars',
+    name: "modal2halfstars"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/modal3stars.png`,
-    name: 'modal3stars',
+    name: "modal3stars"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/modal3halfstars.png`,
-    name: 'modal3halfstars',
+    name: "modal3halfstars"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/modal4stars.png`,
-    name: 'modal4stars',
+    name: "modal4stars"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/modal4halfstars.png`,
-    name: 'modal4halfstars',
+    name: "modal4halfstars"
   },
   {
     src: `${process.env.PUBLIC_URL}./assets/img/stars/modal5stars.png`,
-    name: 'modal5stars',
-  },
+    name: "modal5stars"
+  }
 ];
 
 export default starImages;
